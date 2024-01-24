6a1767b4b1a8577f54e4b664dfcb24d3ff70bdef
YohanesAgengHZP <81618432+YohanesAgengHZP@users.noreply.github.com>
2024-01-24 18:19:30 +0700

    bubble

--- index.js
+++ index.js
@@ -49,18 +49,27 @@ async function sendReport(chatId) {
     const h1Day = moment().clone().subtract(1, 'days').format('YYYY-MM-DD')
     const h2Day = moment().clone().subtract(2, 'days').format('YYYY-MM-DD')
 
-    // Get Total Delivered
-    const qSql = await conn.execute(
+    const qSqlTotal1 = await conn.execute(
       `SELECT SUM(value) AS total
         FROM sms_transaction_summarize_dates
         WHERE summarize_code LIKE 'TRAFFIC_A2P%'
           AND JSON_UNQUOTE(RESULT->'$.status') = 'DELIVERED'
           AND lower(JSON_UNQUOTE(RESULT->'$.aggr_name')) NOT IN ('telin')
-          AND date BETWEEN ? AND ?`,
-      [h2Day, h1Day]
+          AND date = ?`,
+      [h1Day]
     )
 
-    // Get Top 5 Client
+    const qSqlTotal2 = await conn.execute(
+      `SELECT SUM(value) AS total
+        FROM sms_transaction_summarize_dates
+        WHERE summarize_code LIKE 'TRAFFIC_A2P%'
+          AND JSON_UNQUOTE(RESULT->'$.status') = 'DELIVERED'
+          AND lower(JSON_UNQUOTE(RESULT->'$.aggr_name')) NOT IN ('telin')
+          AND date = ?`,
+      [h2Day]
+    )
+
+    // Get Top 5 Client h-1
     const qSql2 = await conn.execute(
       `SELECT JSON_UNQUOTE(RESULT->'$.partner_name') AS partner_name,
               sum(value) AS total
@@ -69,15 +78,31 @@ async function sendReport(chatId) {
             AND lower(JSON_UNQUOTE(RESULT->'$.aggr_name')) NOT IN ('telin')
             AND JSON_UNQUOTE(RESULT->'$.partner_name') IS NOT NULL
             AND JSON_UNQUOTE(RESULT->'$.status') = 'DELIVERED'
-            AND date BETWEEN ? AND ?
+            AND date = ?
         GROUP BY JSON_UNQUOTE(RESULT->'$.partner_name')
         ORDER BY total DESC
         LIMIT 5`,
-      [h2Day, h1Day]
+      [h1Day]
+    )
+
+    // Get Top 5 Client H - 2
+    const qSql3 = await conn.execute(
+      `SELECT JSON_UNQUOTE(RESULT->'$.partner_name') AS partner_name,
+              sum(value) AS total
+        FROM sms_transaction_summarize_dates stsd
+        WHERE summarize_code LIKE 'TRAFFIC_A2P%'
+            AND lower(JSON_UNQUOTE(RESULT->'$.aggr_name')) NOT IN ('telin')
+            AND JSON_UNQUOTE(RESULT->'$.partner_name') IS NOT NULL
+            AND JSON_UNQUOTE(RESULT->'$.status') = 'DELIVERED'
+            AND date = ?
+        GROUP BY JSON_UNQUOTE(RESULT->'$.partner_name')
+        ORDER BY total DESC
+        LIMIT 5`,
+      [h2Day]
     )
 
     // Get Top 5 SenderID
-    const qSql3 = await conn.execute(
+    const qSql4 = await conn.execute(
       `SELECT JSON_UNQUOTE(RESULT->'$.addr_src_digit') AS addr_src_digit,
               sum(value) AS total
         FROM sms_transaction_summarize_dates stsd
@@ -85,39 +110,76 @@ async function sendReport(chatId) {
             AND lower(JSON_UNQUOTE(RESULT->'$.aggr_name')) NOT IN ('telin')
             AND JSON_UNQUOTE(RESULT->'$.addr_src_digit') IS NOT NULL
             AND JSON_UNQUOTE(RESULT->'$.status') = 'DELIVERED'
-            AND date BETWEEN ? AND ?
+            AND date = ?
         GROUP BY JSON_UNQUOTE(RESULT->'$.addr_src_digit')
         ORDER BY total DESC
         LIMIT 5`,
-      [h2Day, h1Day]
+      [h1Day]
     )
 
-    mess += `Report (${h2Day} - ${h1Day}) \n`
-    mess += `-----------\n`
-    mess += `Total Delivered : ${qSql[0][0].total} \n`
-    mess += `-----------\n`
+    const qSql5 = await conn.execute(
+      `SELECT JSON_UNQUOTE(RESULT->'$.addr_src_digit') AS addr_src_digit,
+              sum(value) AS total
+        FROM sms_transaction_summarize_dates stsd
+        WHERE JSON_UNQUOTE(RESULT->'$.aggr_name') IS NOT NULL
+            AND lower(JSON_UNQUOTE(RESULT->'$.aggr_name')) NOT IN ('telin')
+            AND JSON_UNQUOTE(RESULT->'$.addr_src_digit') IS NOT NULL
+            AND JSON_UNQUOTE(RESULT->'$.status') = 'DELIVERED'
+            AND date = ?
+        GROUP BY JSON_UNQUOTE(RESULT->'$.addr_src_digit')
+        ORDER BY total DESC
+        LIMIT 5`,
+      [h2Day]
+    )
+  // Message for Total Delivered
+    let messTotalDelivered = `Report (${h2Day} - ${h1Day}) \n`;
+    messTotalDelivered += `-----------\n`;
+    messTotalDelivered += `Total Delivered - ${h1Day} : ${qSqlTotal1[0][0].total} \n`;
+    messTotalDelivered += `Total Delivered - ${h2Day} : ${qSqlTotal2[0][0].total} \n`;
+    messTotalDelivered += `-----------\n`;
 
     // Set Message Top 5 Client
-    mess += `Top 5 Client\n`
-    for await (const { partner_name, total } of qSql2[0]) {
-      mess += ` • ${partner_name} : ${total}\n`
-    }
+    let messTop5Client1 = `Top 5 Client - ${h1Day} \n`;
+    qSql2[0].forEach(({ partner_name, total }) => {
+      messTop5Client1 += ` • ${partner_name} : ${total}\n`;
+    });
+
+    let messTop5Client2 = `Top 5 Client - ${h2Day}\n`;
+    qSql3[0].forEach(({ partner_name, total }) => {
+      messTop5Client2 += ` • ${partner_name} : ${total}\n`;
+    });
 
     // Set Message Top 5 SenderId
-    mess += `-----------\n`
-    mess += `Top 5 SENDERID\n`
+    let messTop5SenderId1 = `-----------\nTop 5 SENDER ID - ${h1Day}\n`;
+    qSql4[0].forEach(({ addr_src_digit, total }) => {
+      messTop5SenderId1 += ` • ${addr_src_digit} : ${total}\n`;
+    });
 
-    for await (const { addr_src_digit, total } of qSql3[0]) {
-      mess += ` • ${addr_src_digit} : ${total}\n`
-    }
+    let messTop5SenderId2 = `Top 5 SENDER ID - ${h2Day}\n`;
+    qSql5[0].forEach(({ addr_src_digit, total }) => {
+      messTop5SenderId2 += ` • ${addr_src_digit} : ${total}\n`;
+    });
 
-    bot.sendMessage(chatId, mess)
-    return
+    // Array of messages
+    const messages = [
+      messTotalDelivered,
+      messTop5Client1,
+      messTop5Client2,
+      messTop5SenderId1,
+      messTop5SenderId2,
+    ];
+
+    // Send messages using forEach
+    messages.forEach((message) => {
+      bot.sendMessage(chatId, message);
+    });
+
+    return;
   } catch (error) {
-    console.log('Error nih : ', error)
-    conn.rollback()
+    console.log('Error nih : ', error);
+    conn.rollback();
   } finally {
-    conn.release()
+    conn.release();
   }
 }
 
